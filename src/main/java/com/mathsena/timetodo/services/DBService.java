eb9ef4e957c61354da6e54dd98a0ce852a8403e8
MathSena <MathSena07@hotmail.com>
2023-09-29 23:29:04 +0100

    77 Project

--- src/main/java/com/mathsena/timetodo/services/DBService.java
+++ src/main/java/com/mathsena/timetodo/services/DBService.java
@@ -12,43 +12,60 @@ import java.util.List;
 @Service
 public class DBService {
 
-    @Autowired
-    private TodoRepository todoRepository;
+  @Autowired private TodoRepository todoRepository;
 
-    public void instanceDataBase() throws ParseException {
-        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
-        Todo todo1 =
-                new Todo(
-                        null,
-                        "Estudar Spring",
-                        "Estudo de spring boot",
-                        dateFormat.parse("25/03/2022"),
-                        false);
-        Todo todo2 =
-                new Todo(
-                        null,
-                        "Academia",
-                        "Estudo de spring boot",
-                        dateFormat.parse("25/03/2022"),
-                        true);
+  public void instanceDataBase() throws ParseException {
+    SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+    Todo todo1 =
+        new Todo(
+            null,
+            "Acordar antes das 7am",
+            "Definir o alarme para as 6:45am",
+            dateFormat.parse("25/03/2022"),
+                false);
+    Todo todo2 =
+        new Todo(
+            null,
+            "Fazer sua rotina matinal",
+            "Exercícios de alongamento e meditação",
+            dateFormat.parse("25/03/2022"),
+                false);
+    Todo todo3 =
+        new Todo(
+            null,
+            "Ler 10 paginas por dia",
+            "Leitura de um livro escolhido",
+            dateFormat.parse("25/03/2022"),
+                false);
+    Todo todo4 =
+        new Todo(
+            null,
+            "Execitar 1h por dia",
+            "Correr ou ir à academia",
+            dateFormat.parse("25/03/2022"),
+                false);
+    Todo todo5 =
+        new Todo(
+            null,
+            "Tomar 2L de água",
+            "Beber um copo de água a cada hora",
+            dateFormat.parse("25/03/2022"),
+                false);
+    Todo todo6 =
+        new Todo(
+            null,
+            "Seguir uma dieta saudável",
+            "Evitar junk food e comer mais vegetais",
+            dateFormat.parse("25/03/2022"),
+                false);
+    Todo todo7 =
+        new Todo(
+            null,
+            "Dormir antes das 10pm",
+            "Evitar telas uma hora antes de dormir",
+            dateFormat.parse("25/03/2022"),
+            false);
 
-        Todo todo3 =
-                new Todo(
-                        null,
-                        "Leitura",
-                        "Estudo de spring boot",
-                        dateFormat.parse("25/03/2022"),
-                        false);
-
-        Todo todo4 =
-                new Todo(
-                        null,
-                        "Estudo FIAP",
-                        "Estudo de spring boot",
-                        dateFormat.parse("25/03/2022"),
-                        true);
-
-        todoRepository.saveAll(List.of(todo1, todo2, todo3, todo4));
-
-    }
+    todoRepository.saveAll(List.of(todo1, todo2, todo3, todo4, todo5, todo6, todo7));
+  }
 }
